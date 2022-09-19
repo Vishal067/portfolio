7ec704e9f22520381949ed6a9a7342090e635691
Vishal Chouhan <support24@anaxee.com>
2022-09-19 16:35:23 +0530

    minor changes

---
+++ build/precache-manifest.cc4381c4692dba8197119211c2933989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ab95a73621ffc76578f7e354a711ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed971c2962fcd6f18273",
+    "url": "./static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "f2b483511a181d5ef128",
+    "url": "./static/css/main.0b47fe12.chunk.css"
+  },
+  {
+    "revision": "ed971c2962fcd6f18273",
+    "url": "./static/js/2.d442f5cc.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "./static/js/2.d442f5cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b483511a181d5ef128",
+    "url": "./static/js/main.2d103696.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  }
+]);
